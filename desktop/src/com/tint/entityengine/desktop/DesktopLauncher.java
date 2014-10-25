196f2534f7ee76a3ca70606115c0448d68e1cba8
Onni Kosomaa <onni.kosomaa@hotmail.se>
2014-10-25 22:55:36 +0300

    Added LibGDX Project

---
+++ desktop/src/com/tint/entityengine/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tint.entityengine.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tint.entityengine.Launcher;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Launcher(), config);
+	}
+}
