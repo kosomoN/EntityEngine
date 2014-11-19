b4a88e6dadb321078318748397045b0fe7cc8461
Onni Kosomaa <onni.kosomaa@hotmail.se>
2014-11-19 16:01:16 +0200

    Added default package for components to make the json cleaner

--- core/src/com/tint/entityengine/server/EntityTemplateManager.java
+++ core/src/com/tint/entityengine/server/EntityTemplateManager.java
@@ -1,7 +1,6 @@
 package com.tint.entityengine.server;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FilenameFilter;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -10,11 +9,16 @@ import java.util.Map;
 import com.badlogic.ashley.core.Component;
 import com.badlogic.ashley.core.Entity;
 import com.esotericsoftware.jsonbeans.Json;
+import com.esotericsoftware.jsonbeans.JsonReader;
+import com.esotericsoftware.jsonbeans.JsonValue;
+import com.esotericsoftware.jsonbeans.JsonValue.PrettyPrintSettings;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.minlog.Log;
 import com.tint.entityengine.server.entity.components.Initializable;
 
 public class EntityTemplateManager {
+	
+	private static final String COMPONENT_PACKAGE = "com.tint.entityengine.entity.components.";
 	
 	private static Map<String, EntityTemplate> templates = new HashMap<String, EntityTemplate>();
 	
@@ -56,8 +60,24 @@ public class EntityTemplateManager {
     	for(int i = 0; i < files.length; i++) {
     		String name = files[i];
     		try {
+    			//Load the json file
+    			JsonReader reader = new JsonReader();
+    			JsonValue jsonValue = reader.parse(new File("server/entities/" + name));
     			
-    			EntityTemplate template = json.fromJson(EntityTemplate.class, new FileInputStream(new File("server/entities/" + name)));
+    			//Add full package declarations
+    			for(JsonValue component : jsonValue.get("components")) {
+    				JsonValue clazz = component.get("class");
+    				
+    				String className = clazz.asString();
+
+    				//If the class name doesn't contain the package
+    				if(!className.contains(".")) {
+    					clazz.set(COMPONENT_PACKAGE + className);
+    				}
+    			}
+    			
+    			//De-serialize
+    			EntityTemplate template = json.fromJson(EntityTemplate.class, jsonValue.toString());
     			
     			//Remove file extension and add to the map
 				templates.put(name.substring(0, name.lastIndexOf('.')), template);
